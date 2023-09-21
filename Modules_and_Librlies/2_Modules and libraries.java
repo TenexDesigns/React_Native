7225e22f3a1642e1af50b29f74b26e7578665daa
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-21 14:28:16 +0300

    Create 2_Modules and libraries.java

---
+++ Modules_and_Librlies/2_Modules and libraries.java
@@ -0,0 +1,23 @@
+Yes, there are numerous libraries and modules available that can help with various aspects of React Native app development. Here are a few examples:
+
+1. **React Navigation**: This is a popular library for handling navigation in a React Native app. It provides a way to transition between screens and manage navigation history. It's customizable and works on both iOS and Android [Source 0](https://reactnavigation.org/).
+
+2. **React Native Elements**: This is a comprehensive set of pre-built components that are ready to use in your app. The library includes buttons, cards, inputs, and more, all with a consistent design [Source 1](https://reactnativeelements.com/).
+
+3. **Redux**: Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test [Source 2](https://redux.js.org/).
+
+4. **Axios**: Axios is a promise-based HTTP client for the browser and Node.js. It allows you to make HTTP requests from Node.js or XMLHttpRequests from the browser and it supports the Promise API [Source 3](https://axios-http.com/).
+
+5. **Styled-components**: This library allows you to use ES6 and CSS to style your apps without stress. It keeps track of which components are rendered on a page and injects their styles and nothing else, fully automatically [Source 4](https://styled-components.com/).
+
+6. **React Native Vector Icons**: This is a popular library for using vector icons in your app. It includes several different icon sets and allows you to use them as React components [Source 5](https://github.com/oblador/react-native-vector-icons).
+
+7. **React Native Firebase**: Firebase provides a real-time database and backend as a service. The service provides application developers an API that allows application data to be synchronized across clients and stored on Firebase's cloud [Source 6](https://rnfirebase.io/).
+
+8. **React Native Maps**: This library provides a Map component that uses Apple Maps or Google Maps on iOS and Google Maps on Android. Markers and callouts can be easily added and customised [Source 7](https://github.com/react-native-maps/react-native-maps).
+
+9. **React Hook Form**: A library to manage your form state. It's small (size) and performant (re-renders), and provides validation out of the box [Source 8](https://react-hook-form.com/).
+
+10. **Lodash**: Lodash is a JavaScript utility library delivering consistency, modularity, performance, & extras. It helps programmers write more concise and maintainable JavaScript [Source 9](https://lodash.com/).
+
+Remember, it's important to evaluate each library or module to ensure it fits your specific needs and requirements. Always consider the library's community support, documentation, update frequency, and compatibility with your project's React Native version.
