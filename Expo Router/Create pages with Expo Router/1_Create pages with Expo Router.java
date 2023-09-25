2711b73f1cc87b0536f8c59482eab0062ebaa42b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-25 21:50:29 +0300

    Create 1_Create pages with Expo Router.java

---
+++ Expo Router/Create pages with Expo Router/1_Create pages with Expo Router.java
@@ -0,0 +1,25 @@
+Steps
+
+1.npx create-expo-app@latest books --example with-router
+
+2.npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar react-native-gesture-handler
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
