c6a86d818ea4337f789d3a9573de9a5ab7f69baf
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-26 08:18:25 +0300

    Create 1_State_and_form_Manangement.java

---
+++ 1_Ui_librlies_to_use/State_and_form_Manangement/1_State_and_form_Manangement.java
@@ -0,0 +1,14 @@
+
+1.Redux (Optional): 
+If your app requires state management, Redux is a popular choice. You can use it to manage the global state of your application.
+
+GitHub: https://github.com/reduxjs/redux
+
+
+
+
+2.Formik and Yup (For Forms):
+  When working with forms in React Native and Expo, Formik and Yup are excellent choices. Formik helps manage form state, and Yup is a schema validation library.
+
+Formik GitHub: https://github.com/formium/formik
+Yup GitHub: https://github.com/jquense/yup
