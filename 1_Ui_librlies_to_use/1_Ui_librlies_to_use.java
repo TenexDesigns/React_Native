73a0f04a858d0aca045482b6c139b1545ba443a5
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-26 08:13:37 +0300

    Create 1_Ui_librlies_to_use.java

---
+++ 1_Ui_librlies_to_use/1_Ui_librlies_to_use.java
@@ -0,0 +1,25 @@
+
+
+1.React Native Elements:
+React Native Elements is a UI toolkit that offers pre-designed components like buttons, cards, and input fields. It can help you quickly build a consistent and attractive UI.
+
+GitHub: https://github.com/react-native-elements/react-native-elements
+
+
+2.React Native Material
+React Native Material UI: React Native Material UI is one of the most popular UI libraries for React Native. 
+  It implements Material Design guidelines and provides pre-built Material Design components such as Cards, Dialogs, and ListItems[2].
+  Here's a simple example of how to use React Native Material UI:
+
+  
+Yarn
+npm install @react-native-material/core
+
+  
+
+
+
+
+
+
+  
