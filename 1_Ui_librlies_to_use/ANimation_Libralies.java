f86ed6074cb37a706a19e6b5f3b411fc9437ade8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-26 08:15:11 +0300

    Create ANimation_Libralies.java

---
+++ 1_Ui_librlies_to_use/ANimation_Libralies.java
@@ -0,0 +1,24 @@
+1.React Native Gesture Handler:
+This library can be used to implement complex gestures and touch interactions in your app, enhancing the user experience.
+
+GitHub: https://github.com/software-mansion/react-native-gesture-handler
+
+
+  
+
+2.React Native Reanimated:
+
+This library allows for smooth animations and transitions in your React Native and Expo apps. It works well in conjunction with the Gesture Handler library.
+
+GitHub: https://github.com/software-mansion/react-native-reanimated
+
+
+
+
+
+
+  
+3.Lottie: 
+Lottie is a library that enables you to easily add animations to your app. It supports JSON-based animations created with tools like Adobe After Effects.
+
+GitHub: https://github.com/lottie-react-native/lottie-react-native
