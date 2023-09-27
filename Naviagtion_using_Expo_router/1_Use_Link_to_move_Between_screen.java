5e167760a143a31e7b35bb7eb0c14da19356aec2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-27 17:10:31 +0300

    Create 1_Use_Link_to_move_Between_screen.java

---
+++ Naviagtion_using_Expo_router/1_Use_Link_to_move_Between_screen.java
@@ -0,0 +1,16 @@
+
+
+
+
+import { View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Page() {
+  return (
+    <View>
+      <Link href="/about">About</Link>
+    <View/>
+    }
+
+
+This will take you to the About page
