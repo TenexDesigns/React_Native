c78a75cbd7cbf1ec961d06b685a97c3579db4b37
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-27 14:36:54 +0300

    Create More_Shorcuts.java

---
+++ Shorts/More_Shorcuts.java
@@ -0,0 +1,49 @@
+As of my last knowledge update in September 2021, the "React Native Tools" extension for Visual Studio Code provided a set of useful snippets for React Native development. However, the specific features and snippets available in extensions may have changed or expanded since then. Here are some common snippets and features you could expect from extensions like "React Native Tools" or other popular extensions:
+
+1. **StyleSheet.create Shortcut:**
+
+   As you mentioned, the "React Native Tools" extension typically includes a shortcut for quickly generating a StyleSheet object. You can usually use something like `rnss` to create a StyleSheet object, and then Tab or Enter to expand it.
+
+2. **Component Skeletons:**
+
+   Extensions often provide shortcuts for generating component skeletons. Some common ones include:
+
+   - `rnfc` for creating a functional component.
+   - `rncc` for creating a class component.
+   - `rnrfc` for creating a functional component with React Redux integration.
+
+3. **Prop Types Snippets:**
+
+   You might find snippets for quickly generating PropTypes for your component's props, such as `rnpt` for generating PropTypes.
+
+4. **Navigation Snippets:**
+
+   Extensions can offer shortcuts for generating navigation-related code. For example, you might have shortcuts for creating a new screen or a navigator.
+
+5. **Redux Snippets:**
+
+   If you're using Redux in your React Native project, some extensions offer shortcuts for creating Redux actions, reducers, and selectors.
+
+6. **Component Lifecycle Methods:**
+
+   Snippets might be available for quickly inserting common lifecycle methods like `componentDidMount`, `componentDidUpdate`, etc.
+
+7. **Inline Styles:**
+
+   Snippets for generating inline styles can be handy, such as `rnstyle` or `rnstyleconst`.
+
+8. **Component Imports:**
+
+   Extensions can provide shortcuts for quickly importing common React Native components, such as `rnview` for importing `View`.
+
+9. **React Native CLI Commands:**
+
+   Some extensions might offer shortcuts for running common React Native CLI commands like `run-android`, `run-ios`, `start`, etc.
+
+10. **React Native Debugging:**
+
+    Extensions may have debugging features specific to React Native, allowing you to debug your application within VS Code.
+
+To access these snippets and shortcuts, you typically need to type the shortcut code (e.g., `rnss` for StyleSheet.create) and then press the Tab key or another designated trigger key to expand the snippet. Be sure to check the documentation for the specific extension you're using, as features and shortcuts can vary between different extensions and versions.
+
+Please note that the availability and behavior of extensions can change over time, so it's a good idea to check the documentation or the extension's settings within VS Code for the most up-to-date information on available shortcuts and features.
