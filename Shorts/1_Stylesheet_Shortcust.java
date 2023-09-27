dec6ce071e76cc4e7ba3f02c0e04f30abfcc42d9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-27 14:35:45 +0300

    Create 1_Stylesheet_Shortcust.java

---
+++ Shorts/1_Stylesheet_Shortcust.java
@@ -0,0 +1,4 @@
+StyleSheet.create Shortcut: --> rnss
+
+As you mentioned, the "React Native Tools" extension typically includes a shortcut for quickly generating a StyleSheet object.
+  You can usually use something like rnss to create a StyleSheet object, and then Tab or Enter to expand it.
