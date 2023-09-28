4b5f3e30866cb0a944ebcb803dc4ad643b682b1e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-28 12:29:49 +0300

    Create 2_Components_in_Stack.java

---
+++ Expo Router/Components_in_Stack/2_Components_in_Stack.java
@@ -0,0 +1,17 @@
+Expo Router is a file-based router for React Native and web applications that allows you to manage navigation between screens in your app, making it easier for users to move seamlessly between different parts of your app's UI, using the same components on multiple platforms (Android, iOS, and web) [1][3]. It brings the best file-system routing concepts from the web to a universal application, allowing your routing to work across every platform[1]. Expo Router is built on top of React Navigation, which is a popular navigation library for React Native[3]. Some of the components and hooks that Expo Router brings to the table to make navigation easier include:
+
+- Stack: The Stack component allows you to create a stack of screens that can be navigated back and forth[1][3].
+- TabRouter: The TabRouter component allows you to create a tab-based navigation system[2][6].
+- TabScreen: The TabScreen component allows you to define the content of each tab in a tab-based navigation system[2].
+- Drawer: The Drawer component allows you to create a drawer-based navigation system[1].
+- useNavigation: The useNavigation hook allows you to access the navigation object from anywhere in your app[1].
+
+To use Expo Router, you can follow the steps outlined in the Expo Router documentation[1][3]. You can start by adding Expo Router to your project using the command `expo install expo-router` [1]. Then, you can use the various components and hooks provided by Expo Router to define the navigation system for your app. For example, you can use the Stack component to create a stack of screens that can be navigated back and forth, or you can use the TabRouter component to create a tab-based navigation system[1][2]. Overall, Expo Router provides a simple and flexible way to manage navigation in your React Native app, making it easier for users to move between screens and interact with your app.
+
+Citations:
+[1] https://docs.expo.dev/routing/introduction/
+[2] https://blog.expo.dev/introducing-expo-router-v2-3850fd5c3ca1
+[3] https://expo.github.io/router/docs/
+[4] https://expo.github.io/router/docs/features/head/
+[5] https://blog.logrocket.com/native-routing-expo-react-native/
+[6] https://www.notjust.dev/blog/expo-router-2
