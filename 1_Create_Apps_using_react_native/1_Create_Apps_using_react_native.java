db1c6333ee2fbe250511364e2673978d82e80de9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-28 11:12:35 +0300

    Create 1_Create_Apps_using_react_native.java

---
+++ 1_Create_Apps_using_react_native/1_Create_Apps_using_react_native.java
@@ -0,0 +1,28 @@
+Building apps with vanilla React Native involves several steps, from setting up your development environment to creating and testing your app. Here's a step-by-step guide on how to get started:
+
+**1. Set Up Your Development Environment**
+Before you can start building your app, you need to set up your development environment. This involves installing Node.js, the React Native command line interface, a JDK, and Android Studio[^2^].
+- Install Node.js and npm (Node.js package manager). You can download Node.js from the [official website](https://nodejs.org/en/download/) and npm is included in the Node.js installation.
+- Install the React Native command line interface by running `npm install -g react-native-cli` in your terminal[^2^].
+- Install a JDK (Java Development Kit) from the [official Oracle website](https://www.oracle.com/java/technologies/javase-jdk11-downloads.html).
+- Install Android Studio for building your app and testing it on an Android emulator. You can download it from the [official website](https://developer.android.com/studio) and follow the instructions there to install it[^2^].
+
+**2. Create a New React Native App**
+- To create a new React Native project, navigate to the directory where you want to create the project and run `react-native init YourAppName`. Replace `YourAppName` with your desired app name[^7^].
+- This will create a new directory with your app name, with all the necessary files and directories for a basic React Native app[^7^].
+
+**3. Run Your React Native App**
+- Navigate to your app directory by running `cd YourAppName` in your terminal[^7^].
+- To start your app, run `npx react-native run-android` for Android or `npx react-native run-ios` for iOS[^7^].
+- You should see your new app running in your Android emulator or iOS simulator[^7^].
+
+**4. Start Developing Your App**
+- Now that your app is up and running, you can start developing it. Open the `App.js` file in your text editor and start building your app[^7^].
+- Any changes you make to your code can be seen by reloading your app. In your emulator or simulator, press `R` twice quickly for a full reload[^2^].
+
+Remember, developing apps with vanilla React Native requires you to manage your own build configurations and handle native code. This gives you more flexibility and control, but it also means more complexity compared to using a managed platform like Expo[^3^][^5^].
+
+
+
+
+  
