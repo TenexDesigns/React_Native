adc36f592bf82bdbc58238f0c1ff7ec0553e0a25
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-28 14:15:52 +0300

    Update 1_Create_expo_apps.java

--- Expo Router/1_Create_Expo_apps/1_Create_expo_apps.java
+++ Expo Router/1_Create_Expo_apps/1_Create_expo_apps.java
@@ -5,4 +5,13 @@ Steps
 2.npx expo install expo-router react-native-safe-area-context react-native-screens expo-linking expo-constants expo-status-bar react-native-gesture-handler
 
 
+
+
+  Incase of any error , run the follwing commands
+
+   4 npx expo-doctor
+   5 npx expo install --check
+   6 npx expo install --fix
+
+
   
